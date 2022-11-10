52722b7f6f4560d33b338c161993f5e64d5f90e3
jcwooo <jcwooo@192.168.123.104>
2022-11-10 22:21:04 +0900

    test commit2

--- PAYROLL/src/main/java/net/cj/payroll/CjhrPayrollApplication.java
+++ PAYROLL/src/main/java/net/cj/payroll/CjhrPayrollApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CjhrPayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CjhrPayrollApplication.class, args); 
+		SpringApplication.run(CjhrPayrollApplication.class, args);
 	}
 
 }
