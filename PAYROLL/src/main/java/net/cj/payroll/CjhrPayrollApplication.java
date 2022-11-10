f5f3516094aecd83357b6e365678ac49dcff4068
jcwooo <jcwooo@192.168.123.104>
2022-11-10 22:10:35 +0900

    first commit

---
+++ PAYROLL/src/main/java/net/cj/payroll/CjhrPayrollApplication.java
@@ -0,0 +1,13 @@
+package net.cj.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjhrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjhrPayrollApplication.class, args);
+	}
+
+}
---
+++ PAYROLL/src/test/java/net/cj/payroll/CjhrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cj.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjhrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
